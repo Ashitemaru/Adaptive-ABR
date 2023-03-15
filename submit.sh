2b92a961a63e16780895eeef0965ba05c0ade53b
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2023-03-16 02:06:40 +0800

    Feature: Add submit script

---
+++ submit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/zsh
+if [ 1 -ne $# ]; then
+    echo "Misuse of submit script."
+    exit 1
+fi
+
+echo "Use BLACK to format all Python files."
+black ./learning_to_adapt
+black ./src
+echo "Finish formatting"
+
+echo "Export conda environment"
+conda env export > ./environment.yml
+echo "Finish exporting"
+
+echo "Push code to GitHub"
+git add .
+git commit -m $1
+git push
+echo "Script over"
